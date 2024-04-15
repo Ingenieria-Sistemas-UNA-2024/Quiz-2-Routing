c088cd143ffee5f4f3e11d7f656deb9c93daa54a
Raul <raulbejarano2002@gmail.com>
2024-04-14 22:26:17 -0600

    Mejora css

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -17,9 +17,9 @@ function ProfileCard ({ userID }: { userID: number }) {
     fetchData();
   }, []);
   return (
-    <div className="flex justify-center items-start min-h-screen">
-      <div className="md:grid grid-cols-4 grid-rows-2 bg-white gap-2 p-4 rounded-xl">
-        <div className="md:col-span-3 h-48 shadow-xl p-4 space-y-2 p-3">
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <div className="justify-center items-center md:grid grid-cols-4 grid-rows-2 bg-white gap-2 p-4 rounded-x1">
+        <div className="justify-center items-center md:col-span-3 shadow-xl p-4 m-2 space-y-2 bg-gray-200">
           <ProfileField label="Nombre" value={user?.name} />
           <ProfileField label="Apellidos" value={user?.lastname} />
           <ProfileField label="Privacidad" value={user?.privacy} />
--- src/components/ProfileDetails.tsx
+++ src/components/ProfileDetails.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ProfileField from "./ProfileField";
-import { User } from "@/model/user"
+import { User } from "@/model/user";
 import { getUser } from "@/lib/apiClientConsumer";
 
-function ProfileDetails ({ userID }: { userID: number }) {
+function ProfileDetails({ userID }: { userID: number }) {
   const [user, setUser] = useState<User | null>(null);
   useEffect(() => {
     async function fetchData() {
@@ -19,21 +19,24 @@ function ProfileDetails ({ userID }: { userID: number }) {
   return (
     <div className="flex justify-center items-start min-h-screen">
       <div className="md:grid grid-cols-4 grid-rows-2 bg-white gap-2 p-4 rounded-xl w-full max-w-4xl">
-        <div className="md:col-span-1">
-          <div className="flex w-full h-full relative"></div>
-        </div>
-        <div className="md:col-span-3 p-4 space-y-2 p-3">
+        <div className="md:col-span-3 p-4 space-y-2 p-3 bg-gray-200">
           <ProfileField label="Nombre" value={user?.name} />
           <ProfileField label="Apellidos" value={user?.lastname} />
           <ProfileField label="Privacidad" value={user?.privacy} />
           <ProfileField label="Rol" value={user?.role} />
           <ProfileField label="Género" value={user?.gender} />
-          <ProfileField label="Lugar de residencia" value={user?.place_of_residence}/>
-          <ProfileField label="Fecha de nacimiento" value={user?.birthdate.toString()} />
+          <ProfileField
+            label="Lugar de residencia"
+            value={user?.place_of_residence}
+          />
+          <ProfileField
+            label="Fecha de nacimiento"
+            value={user?.birthdate.toString()}
+          />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default ProfileDetails;
